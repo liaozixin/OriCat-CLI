302cfbcd68a87a8b3eb8e58af56e4b66c5e7194a
liaozixin <1571155318@qq.com>
2026-07-01 09:12:44 +0800

    修改项目名

--- src/main.py
+++ src/main.py
@@ -1,13 +1,2 @@
-from textual.app import App
-from textual.widgets import Header, Footer, Static
 
-
-class MyApp(App):
-    def compose(self):
-        yield Header()
-        yield Static("Hello Textual!")
-        yield Footer()
-
-
-if __name__ == "__main__":
-    MyApp().run()
+class 
